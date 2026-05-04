2db546be9abf4aa42c33216801b3750022b1b3cf
Isaac Aragón <isaacarag@uamv.edu.ni>
2026-05-04 15:02:43 -0600

    ref: Imports sin usar, NavHost mal identado y modifier sin uso.

--- app/src/main/java/com/orbix/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/orbix/ui/navigation/AppNavigation.kt
@@ -1,10 +1,7 @@
 package com.orbix.ui.navigation
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -14,46 +11,46 @@ import com.orbix.ui.screen.LoginScreen
 import com.orbix.ui.screen.UserReviewScreen
 
 @Composable
-fun AppNavigation(modifier: Modifier) {
+fun AppNavigation() {
     val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = Login)
-        {
-            composable<Login> {
-                LoginScreen(
-                    onLogin = {
-                        navController.navigate(Home) {
-                        }
+    NavHost(navController = navController, startDestination = Login)
+    {
+        composable<Login> {
+            LoginScreen(
+                onLogin = {
+                    navController.navigate(Home) {
                     }
-                )
-            }
-            composable<Home> {
-                HomeScreen(
-                    onLogout = {
-                        navController.popBackStack()
-                    },
-                    onNavigateToCarReview = {
-                        navController.navigate(CarReview)
-                    },
-                    onNavigateToUserReview = {
-                        navController.navigate(UserReview)
-                    }
-
-                )
-            }
-            composable<CarReview> {
-                CarReviewScreen(
-                    onReviewSubmitted = {
-                        navController.popBackStack()
-                    }
-                )
-            }
-            composable<UserReview> {
-                UserReviewScreen(
-                    onReviewSubmitted = {
-                        navController.popBackStack()
-                    }
-                )
-            }
+                }
+            )
         }
+        composable<Home> {
+            HomeScreen(
+                onLogout = {
+                    navController.popBackStack()
+                },
+                onNavigateToCarReview = {
+                    navController.navigate(CarReview)
+                },
+                onNavigateToUserReview = {
+                    navController.navigate(UserReview)
+                }
+
+            )
+        }
+        composable<CarReview> {
+            CarReviewScreen(
+                onReviewSubmitted = {
+                    navController.popBackStack()
+                }
+            )
+        }
+        composable<UserReview> {
+            UserReviewScreen(
+                onReviewSubmitted = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
 
 }
